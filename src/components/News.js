6abada0ff79d5c6371cd977a2b9449bf8e3d42f7
SAVAN JADAV <savan.p.jadav@gmail.com>
2024-04-04 13:06:30 +0530

    Day4-4

--- src/components/News.js
+++ src/components/News.js
@@ -33,13 +33,17 @@ export class News extends Component {
   }
 
   async updateNews() {
+    this.props.setProgress(10);
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0c075515b6aa4cf0a42bb46fd4cf6ccd&page=${this.state.page}&pagesize=${this.props.pageSize}`;
     let data = await fetch(url);
+    this.props.setProgress(30);
     let parsedData = await data.json();
+    this.props.setProgress(70);
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults
     })
+    this.props.setProgress(100);
   }
 
   async componentDidMount() {
